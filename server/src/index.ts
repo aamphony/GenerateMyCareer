c2a12e7b1e3a1544b23d552c88e5b39d3a97a361
thealexay <aamphony@gmail.com>
2024-06-23 20:43:30 -0700

    Dockerize server for dev

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,10 +2,13 @@ import express from 'express';
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
+    console.log('Hello World');
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
